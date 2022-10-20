22e98a111012f0c11bea2683c33ee38838be431b
Sárándi <sarandi.balint@gmail.com>
2022-10-20 11:15:27 +0200

    versenyzok/sportagszam

--- src/hu/petrik/versenyeredmenyek/Main.java
+++ src/hu/petrik/versenyeredmenyek/Main.java
@@ -5,13 +5,50 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-    private static Map<String, List<Eredmeny>> sportagEredmenyek = new HashMap<>();
+    private static final Map<String, List<Eredmeny>> sportagEredmenyek = new HashMap<>();
     public static void main(String[] args) {
         String fajlNev = "eredmenyek.txt";
         try {
             beolvas(fajlNev);
+            kiiras();
+            System.out.println("--------------------------");
+            System.out.printf("Sportágak száma: %s\n", getSportagakSzama());
+            System.out.printf("Az olimpián %d versenyző vett részt\n", getVersenyzokSzama());
+
+
+
+
+            System.out.print("--------------------------");
         } catch (FileNotFoundException e) {
             System.out.printf("Nem találhato a %s fájl",fajlNev);
+        }
+    }
+
+    private static int getVersenyzokSzama() {
+        List<String> versenyzok = new ArrayList<>();
+        for (Map.Entry<String, List<Eredmeny>> entry: sportagEredmenyek.entrySet()) {
+            List<Eredmeny> eredmenyek = entry.getValue();
+            for (Eredmeny e: eredmenyek) {
+                if (!versenyzok.contains(e.getNev())){
+                    versenyzok.add(e.getNev());
+                }
+            }
+        }
+        return versenyzok.size();
+    }
+
+    private static int getSportagakSzama() {
+        return sportagEredmenyek.keySet().size();
+    }
+
+    private static void kiiras() {
+        for (Map.Entry<String, List<Eredmeny>> entry : sportagEredmenyek.entrySet()) {
+            String sportag = entry.getKey();
+            List<Eredmeny> eredmenyek = entry.getValue();
+            System.out.println(sportag);
+            for (Eredmeny eredmeny : eredmenyek){
+                System.out.println("\t" + eredmeny);
+            }
         }
     }
 
